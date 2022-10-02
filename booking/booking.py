a5264e0ac04036e12944d614f36d1aeaa80f91f1
Jules <jmarchepro@gmail.com>
2022-10-02 14:53:51 +0200

    Ajout de commentaires

--- booking/booking.py
+++ booking/booking.py
@@ -19,6 +19,11 @@ def home():
 def get_json():
    return make_response(jsonify(bookings), 200)
 
+
+# Fonction crée par Tournier Quentin et Marche Jules
+# But: Afficher toutes les réservations d'un Utilisateur
+# En entrée: UserId(path)
+# En sortie: Un tableau de toutes les réservations
 @app.route("/bookings/<userid>", methods=['GET'])
 def get_booking_for_user(userid):
    for booking in bookings:
@@ -26,6 +31,11 @@ def get_booking_for_user(userid):
          return make_response(jsonify(booking), 200)
    return make_response(jsonify({"error":"booking with this id not found"}),400)
 
+
+# Fonction crée par Tournier Quentin et Marche Jules
+# But: Ajouter une réservation à un utilisateur
+# En entrée: UserId(path)
+# En sortie: Un message stipulant l'ajout de la réservation
 @app.route("/bookings/<userid>", methods=['POST'])
 def add_booking_byuser(userid):
    req = request.get_json()
